8a299f5c2b04a43d079506e66b925e0d9811a439
florimondmanca <florimond.manca@gmail.com>
2019-05-12 15:01:13 +0200

    setup bumpversion

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
-import ast
 import io
-import re
 import os
 from setuptools import find_packages, setup
 
-DEPENDENCIES = []
 EXCLUDE_FROM_PACKAGES = ["contrib", "docs", "tests*"]
 CURDIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -12,18 +9,9 @@ with io.open(os.path.join(CURDIR, "README.md"), "r", encoding="utf-8") as f:
     README = f.read()
 
 
-def get_version():
-    main_file = os.path.join(CURDIR, "starlette_auth_toolkit", "__init__.py")
-    _version_re = re.compile(r"__version__\s+=\s+(?P<version>.*)")
-    with open(main_file, "r", encoding="utf8") as f:
-        match = _version_re.search(f.read())
-        version = match.group("version") if match is not None else '"unknown"'
-    return str(ast.literal_eval(version))
-
-
 setup(
     name="starlette-auth-toolkit",
-    version=get_version(),
+    version="0.0.1",
     author="Florimond Manca",
     author_email="florimond.manca@gmail.com",
     description=(
@@ -36,7 +24,7 @@ setup(
     packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
     include_package_data=True,
     zip_safe=False,
-    install_requires=DEPENDENCIES,
+    install_requires=[],
     python_requires=">=3.6",
     # license and classifier list:
     # https://pypi.org/pypi?%3Aaction=list_classifiers
